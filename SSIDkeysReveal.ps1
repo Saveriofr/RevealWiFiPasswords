db9edad5e9afbd56b66309d36a6699655dda0b62
Saverio <saveriofr@outlook.com>
2022-02-10 16:51:09 +0100

    Powershell WiFi password reveal
    A small script that reveals all wifi passwords in windows computer

--- SSIDkeysReveal.ps1
+++ SSIDkeysReveal.ps1
@@ -6,6 +6,7 @@ $ssidlist = netsh wlan show profile
 
 # format the variable as a list
 $newssidlist = $ssidlist | ForEach-Object{ $_.Split(':')[1]; }  2>$null
+
 #count ssid list and put it in a variable
 $counter = $newssidlist.Count
 
@@ -14,9 +15,15 @@ while($counter -ne 0)
 {
     #new variable with ssid name from list
     $ssid = $newssidlist[$counter-1]
+
+    #format every single ssid name
     $ssid = $ssid.trim()
+
+    #show pass in clear and format
     $netshow = netsh wlan show profile name="$ssid" key=clear
     $keycontent = $netshow | Select-String Contenuto 
+
+    #check if the passowrd is present and write it in a file
     if ( $keycontent -eq $null) 
     {
     Write-Output " " >> passwords.txt
@@ -24,18 +31,11 @@ while($counter -ne 0)
     Write-Output "Contenuto chiave: Nessuna chiave" >> passwords.txt
     }
         else {
-            # show only ssid and password for each ssid
     Write-Output " " >> passwords.txt
     Write-Output "Nome rete: $ssid " >> passwords.txt 
     $netshow | Select-String Contenuto >> passwords.txt
-        
-        }
+         }
     
-
-    
-
-    
-
     $counter = $counter - 1
 
 }
